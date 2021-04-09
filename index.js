bbc29e8c1474afe59640d76a281e3e72bbf1ad8f
Emily Wilbur <ewilbur94@gmail.com>
2021-04-09 19:25:58 -0400

    fucntion to generate markdown for readme

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
-// TODO: Include packages needed for this application
+//  packages needed for this application
 
-const inquirer = require(`inquirer`);
-const fs = require(`fs`);
-const generateMarkdown = require(`./utils/generateMarkdown`);
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 
-// TODO: Create an array of questions for user input
-const questions = [];
-
-inquirer
-  .prompt([
+// questions for user input
+const questions = [
     {
       type: 'input',
       name: 'title',
@@ -60,14 +57,18 @@ inquirer
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    fs.writeFile('README.md', data, (err) => err ? console.log)
-
+    fs.writeFile(fileName, data, (err) => err ? console.log(err) : console.log
+    ("README generated successfully!"));
 }
+
+
+
 
 // TODO: Create a function to initialize app
 function init() {
-    inquirer.prompt(questions).then(response) => {
+    inquirer.prompt(questions).then((response) => {
         console.log(response);
+        writeToFile('README.md', generateMarkdown(response));
     })
 }
 
