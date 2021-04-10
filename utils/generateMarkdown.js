1a475a856f59b7b14ec376d2f5fcf6de269970af
Emily Wilbur <ewilbur94@gmail.com>
2021-04-09 21:45:15 -0400

    finalized changes in markdown

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -7,12 +7,10 @@ function renderLicenseBadge(license) {
   
 }
 
-//  a function that returns the license link
-// If there is no license, return an empty string
+
 function renderLicenseLink(license) {}
 
-//  function that returns the license section of README
-// If there is no license, return an empty string
+
 function renderLicenseSection(license) {}
 
 // function to generate markdown for README
